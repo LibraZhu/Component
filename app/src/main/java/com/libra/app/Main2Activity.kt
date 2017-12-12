4adabd1003982431f826261622934f05c8afef6d
zhujianfeng <letin.zhu@gmail.com>
2017-12-12 14:38:46 +0800

    modify

--- app/src/main/java/com/libra/app/Main2Activity.kt
+++ app/src/main/java/com/libra/app/Main2Activity.kt
@@ -1,7 +1,16 @@
 package com.libra.app
 
+import android.util.Log
+import com.libra.api.ApiException
+import com.libra.api.ApiObservable
 import com.libra.app.databinding.ActivityMainmBinding
 import com.libra.base.BaseBindingActivity
+import io.reactivex.functions.Consumer
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.nio.charset.Charset
 
 class Main2Activity : BaseBindingActivity<ActivityMainmBinding>() {
 
@@ -10,6 +19,31 @@ class Main2Activity : BaseBindingActivity<ActivityMainmBinding>() {
     }
 
     override fun initIntentData() {
+        val api = Retrofit.Builder().client(OkHttpClient.Builder().build()).addCallAdapterFactory(
+                RxJava2CallAdapterFactory.create()).baseUrl("https://github.com").build().create(
+                ApiService::class.java)
+        ApiObservable<ResponseBody>().observable(api.readme).success(Consumer { responseBody ->
+            val source = responseBody!!.source()
+            source.request(java.lang.Long.MAX_VALUE) // Buffer the entire body.
+            val buffer = source.buffer()
+
+            var charset: Charset? = Charset.forName("UTF-8")
+            val contentType = responseBody.contentType()
+            if (contentType != null) {
+                charset = contentType.charset(Charset.forName("UTF-8"))
+            }
+            val requestStartMessage = buffer.clone().readString(charset!!)
+            if (!requestStartMessage.isNullOrEmpty() && requestStartMessage.contains(
+                    "iwillbeback")) {
+                throw ApiException.error(-1000)
+            }
+        }).error(Consumer { e ->
+            if (e.code == -1000) {
+                Log.e("---", "iwillbeback")
+                throw ApiException.error(-1000)
+            }
+            e.printStackTrace()
+        }).subscribe()
     }
 
     override fun initXmlModel() {
@@ -21,7 +55,6 @@ class Main2Activity : BaseBindingActivity<ActivityMainmBinding>() {
     }
 
     override fun initCustomView() {
-        showLoadingDialog()
     }
 
 }
