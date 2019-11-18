79ebb2210f86b1113977dc1f8a012f6092836a4b
LibraZhu <letin.zhu@gmail.com>
2019-11-18 17:48:05 +0800

    1.1.0

--- base_kotlin/src/main/java/com/libra/api/ApiException.kt
+++ base_kotlin/src/main/java/com/libra/api/ApiException.kt
@@ -1,10 +1,13 @@
 package com.libra.api
 
+import android.text.TextUtils
+import org.json.JSONObject
 import retrofit2.HttpException
-import java.lang.Exception
+import retrofit2.Response
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
+import java.nio.charset.Charset
 
 /**
  * Created by libra on 2017/8/1.
@@ -12,11 +15,40 @@ import java.net.UnknownHostException
 
 class ApiException : Exception {
     var code: Int = 0
+    var errorResponseMessage: String? = null
+    private var response: Response<*>? = null
+
+    override fun getLocalizedMessage(): String {
+        if (!TextUtils.isEmpty(errorResponseMessage)) {
+            return errorResponseMessage!!
+        }
+        return super.getLocalizedMessage()
+    }
 
     private constructor(throwable: Throwable) : super(throwable) {}
 
     private constructor(code: Int, message: String?) : super(message ?: "") {
         this.code = code
+    }
+
+    private constructor(code: Int, message: String?, response: Response<*>?) : super(message
+            ?: "") {
+        this.code = code
+        this.response = response
+        try {
+            val json = response?.errorBody()?.source()?.readString(Charset.forName("UTF-8"))
+            if (!TextUtils.isEmpty(json)) {
+                val jsonObj = JSONObject(json)
+                if (jsonObj.has("status_code")) {
+                    this.code = jsonObj.optInt("status_code")
+                }
+                if (jsonObj.has("message")) {
+                    this.errorResponseMessage = jsonObj.optString("message")
+                }
+            }
+        } catch (e: Throwable) {
+
+        }
     }
 
     companion object {
@@ -40,13 +72,17 @@ class ApiException : Exception {
             return ApiException(code, message)
         }
 
+        fun error(code: Int, message: String?, response: Response<*>?): ApiException {
+            return ApiException(code, message, response)
+        }
+
         fun error(throwable: Throwable): ApiException {
             if (throwable is SocketTimeoutException) {
                 return error(ERROR_HTTP_CONNECT_TIMEOUT)
             } else if (throwable is UnknownHostException || throwable is ConnectException) {
                 return error(ERROR_HTTP_HOST_UNKNOWN)
             } else if (throwable is HttpException) {
-                return error(ERROR_HTTP_CONNECT, throwable.message)
+                return error(throwable.code(), throwable.message, throwable.response())
             }
             return ApiException(throwable)
         }
